65089453a2ca95de3dfb7d6e4db1c84bfe503342
Mikhail Chirkov <chirkovmtest@yandex.ru>
2020-04-12 18:23:54 +0300

    Initial Commit

---
+++ trader-stat-app/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  trader-stat-app
+//
+//  Created by user on 12.04.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
